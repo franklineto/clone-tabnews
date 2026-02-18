7d61c9df018f6a4b315908f4f56f1b50690f217a
franklineto <franklin.oliveira@outlook.com>
2026-02-18 19:13:01 +0000

    Add scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Isso é uma demonstração de deploy para a Kenia</h1>
+function Home() {
+  return <h1>Isso é uma demonstração de deploy para a Kenia</h1>;
 }
 
 export default Home;
