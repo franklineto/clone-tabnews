fd6a6fa02625f595c75597123598c2c3cee55bd0
franklineto <franklin.oliveira@outlook.com>
2026-02-25 23:15:35 +0000

    buscando versao bd

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,14 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS COL1;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SELECT version();");
+  const databaseVersionValue = databaseVersionResult.rows[0].version;
+
   response.status(200).json({
-    chave: "são acima da média!",
+    updated_at: updatedAt,
+    database_version: databaseVersionValue,
   });
 }
 
