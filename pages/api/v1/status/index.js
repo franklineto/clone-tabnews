eafbeb431a3f8e0c51fe8d5ef19c56741e332bf5
franklineto <franklin.oliveira@outlook.com>
2026-02-25 23:59:53 +0000

    ajuste version_database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,12 +3,16 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SELECT version();");
-  const databaseVersionValue = databaseVersionResult.rows[0].version;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   response.status(200).json({
     updated_at: updatedAt,
-    database_version: databaseVersionValue,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
