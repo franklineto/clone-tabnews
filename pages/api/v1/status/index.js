3366f8fc56b9dc96b9bc2f85ef4f2e7ab588c93b
franklineto <franklin.oliveira@outlook.com>
2026-02-26 03:35:03 +0000

    incluindo conexões ativas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,12 +5,25 @@ async function status(request, response) {
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db' AND state = 'active';",
+  );
+  const databaseOpenedConnetionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnetionsValue,
       },
     },
   });
