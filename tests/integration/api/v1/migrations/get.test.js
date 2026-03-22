c73e5f4514a10cfc64c641f334891529b11ebeb8
Franklin <franklin.oliveira@outlook.com>
2026-03-22 01:37:02 +0000

    primeiro commit

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -11,6 +11,7 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  //sdfsdfsdf
   //console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
