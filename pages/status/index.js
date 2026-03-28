137ca798eb1e88605b8fbf5538a4ea4775aa89f7
Franklin <franklin.oliveira@outlook.com>
2026-03-28 17:02:35 +0000

    refactor: initial adjust status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,9 +1,20 @@
-<CapsLock texto="teste de texto" />;
+import useSWR from "swr";
 
-function CapsLock(propriedades) {
-  console.log(propriedades);
+async function fetchStatus() {
+  const response = await fetch("/proxy/3000/api/v1/status");
+  const responseBody = await response.json();
+  return responseBody;
 }
 
 export default function StatusPage() {
-  return <h1>Status</h1>;
+  const response = useSWR("status", fetchStatus);
+  //console.log(response.isLoading);
+  //console.log(response.data);
+
+  return (
+    <>
+      <h1>Status</h1>
+      <pre>{JSON.stringify(response.data, null, 2)}</pre>
+    </>
+  );
 }
