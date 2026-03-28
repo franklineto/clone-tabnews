f5cec5664d68e98ee5f6d47c099fb9d4686b3db3
Franklin <franklin.oliveira@outlook.com>
2026-03-28 17:57:06 +0000

    refactor: adjust status page final

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,16 +1,17 @@
 import useSWR from "swr";
 
 async function fetchStatus() {
-  const response = await fetch("/proxy/3000/api/v1/status");
+  const response = await fetch(
+    process.env.NODE_ENV === "development"
+      ? "/proxy/3000/api/v1/status"
+      : "/api/v1/status",
+  );
   const responseBody = await response.json();
   return responseBody;
 }
 
 export default function StatusPage() {
   const response = useSWR("status", fetchStatus);
-  //console.log(response.isLoading);
-  //console.log(response.data);
-
   return (
     <>
       <h1>Status</h1>
