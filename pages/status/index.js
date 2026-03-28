e976369ffaebb405f0c82830721643ec80ff4ccd
Franklin <franklin.oliveira@outlook.com>
2026-03-28 20:40:50 +0000

    refactor: adjust status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,21 +1,59 @@
 import useSWR from "swr";
 
-async function fetchStatus() {
-  const response = await fetch(
-    process.env.NODE_ENV === "development"
+const url = process.env.NODE_ENV === "development"
       ? "/proxy/3000/api/v1/status"
-      : "/api/v1/status",
-  );
+      : "/api/v1/status"
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
   const responseBody = await response.json();
   return responseBody;
 }
 
 export default function StatusPage() {
-  const response = useSWR("status", fetchStatus);
   return (
     <>
       <h1>Status</h1>
-      <pre>{JSON.stringify(response.data, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(response.data, null, 2)}</pre> */}
+      <UpdatedAt />
+      <h1>Database</h1>
+      <DatabaseStatus />
     </>
   );
+}
+
+function UpdatedAt(){
+  const {isLoading, data} = useSWR(url, fetchAPI, {
+    refreshInterval: 2000,
+    //dedupingInterval: 500,
+  });
+
+  let UpdatedAtText = "Carregando..."
+
+  if (!isLoading && data){
+    UpdatedAtText = new Date(data.updated_at).toLocaleString('pt-BR');
+  }
+  return <div>Ultima atualização: {UpdatedAtText}</div>
+}
+
+function DatabaseStatus(){
+  const {isLoading, data} = useSWR(url, fetchAPI, {
+    refreshInterval: 2000,
+    //dedupingInterval: 500,
+  });
+
+  let Version =  "Carregando..."
+  let MaxConnections =  "Carregando..."
+  let OpennedConnections =  "Carregando..."
+
+  if (!isLoading && data){
+    Version = data.dependencies.database.version;
+    MaxConnections = data.dependencies.database.max_connections;
+    OpennedConnections =  data.dependencies.database.opened_connections
+  }
+  return <>
+          <div>Versão: {Version}</div>
+          <div>Máximo Conexões Simultâneas: {MaxConnections}</div>
+          <div>Conexões Abertas: {OpennedConnections}</div>
+          </>
 }
