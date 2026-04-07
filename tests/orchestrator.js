1cd61af4e655060efdfb5b3aa108fe6f17dbb888
Franklin <franklin.oliveira@outlook.com>
2026-04-07 19:08:04 +0000

    feat: add `runPendingMigrations` method to `orchestrator`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,5 +1,6 @@
 import database from "infra/database.js";
 import retry from "async-retry";
+import migrator from "models/migrator.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -22,8 +23,15 @@ async function waitForAllServices() {
 async function clearDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
+
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
+  runPendingMigrations,
 };
 export default orchestrator;
