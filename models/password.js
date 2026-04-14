7a81d5a5cda14608516a73eaa2591cc2ab5be2ed
Franklin <franklin.oliveira@outlook.com>
2026-04-14 02:58:33 +0000

    refactor: adjust for tests and new method update

--- models/password.js
+++ models/password.js
@@ -7,11 +7,6 @@ async function hash(password) {
 
 function getNumbersOfRounds() {
   return process.env.NODE_ENV === "production" ? 14 : 1;
-  //let rounds = 1;
-  //if (process.env.NODE_ENV === 'production') {
-  //    rounds = 14;
-  //}
-  //return rounds;
 }
 
 async function compare(provivedPassword, storedPassword) {
